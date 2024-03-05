62f2fbbe987aa4e3f76fa458ddb859002d9cbe34
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-03-05 22:01:38 +0800

    created the capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,2 +1,10 @@
+function capitalize(word) {
+    if (!word || typeof word !== 'string') {
+        return '';
+    }
+
+    word.toLowerCase();
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
 module.exports = capitalize;
